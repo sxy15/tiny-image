c64a91ded9b04b140cdeb34b14330220c5be72f8
sxy15 <sxy_19921115@163.com>
2025-02-26 13:59:05 +0800

    feat: tauri v2

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import addImage from './components/add-image.vue';
+  import imgList from './components/img-list.vue';
+</script>
+
+<template>
+  <div>
+    <add-image />
+    <img-list />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/add-image.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrap" @click="selectFolder">
+    <img class="add-icon" src="../assets/add_icon.png" alt="">
+  </div>
+  {{ imageList }}
+
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from 'vue'
+
+const imageList: Ref<any[]> = ref([])
+
+const selectFolder = async () => {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  padding: 20px;
+  border: 1.5px dashed #33f343;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.add-icon {
+  width: 80px;
+  height: 80px;
+  display: block;
+  margin: 0 auto;
+}
+</style>
